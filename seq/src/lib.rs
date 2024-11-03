b27c9e9ace32f59ffafcab18f29e810b5eeae246
Antoine Pouillaude <pouillaude.a@gmail.com>
2024-11-03 22:34:35 +0100

    Solve exercise 4 and 5 in the Seq macro.

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,9 +1,7 @@
-use std::io::BufWriter;
-
 use proc_macro::TokenStream;
 use proc_macro2::{TokenTree as TT, TokenStream as TokenStream2};
 
-use quote::{quote, ToTokens, TokenStreamExt};
+use quote::TokenStreamExt;
 use syn::buffer::{TokenBuffer, Cursor};
 use syn::parse_macro_input;
 use syn::parse::{Parse, ParseStream};
@@ -12,9 +10,8 @@ use syn::parse::{Parse, ParseStream};
 pub fn seq(input: TokenStream) -> TokenStream {
     let seq = parse_macro_input!(input as Seq);
 
-    eprintln!("{:#?}", seq);
-
-    seq.expand().into()
+    let res = seq.expand().into();
+    res
 }
 
 #[derive(Debug)]
@@ -23,6 +20,7 @@ struct Seq {
     token_in: syn::Token![in],
     lower_bound: syn::LitInt,
     token_2dots: syn::Token![..],
+    token_eq: Option<syn::Token![=]>,
     upper_bound: syn::LitInt,
     brace_token: syn::token::Brace,
     content: TokenStream2,
@@ -36,6 +34,7 @@ impl Parse for Seq {
             token_in: input.parse()?,
             lower_bound: input.parse()?,
             token_2dots: input.parse()?,
+            token_eq: input.parse().ok(),
             upper_bound: input.parse()?,
             brace_token: syn::braced!(content in input),
             content: content.parse()?,
@@ -58,8 +57,13 @@ impl Seq {
 
         match (lower_bound.base10_parse(), upper_bound.base10_parse()) {
             (Ok(lb), Ok(ub)) => {
-                TokenStream2::from_iter(
-                    (lb..ub).into_iter().map(|val| Self::replace(cursor, &ident, proc_macro2::Literal::i32_unsuffixed(val))))
+                let (matched, stream) = Self::partial_match(cursor, &ident, lb, ub);
+                if matched {
+                    stream
+                } else {
+                    TokenStream2::from_iter(
+                        (lb..ub).into_iter().map(|val| Self::replace(cursor, &ident, val)))
+                }
             },
             (Err(mut lb_e), Err(ub)) => {
                 lb_e.combine(ub);
@@ -69,19 +73,78 @@ impl Seq {
         }
     }
 
-    fn replace(mut cursor: Cursor, ident: &syn::Ident, lit: proc_macro2::Literal) -> TokenStream2 {
+    fn partial_match(mut cursor: Cursor, ident: &syn::Ident, lb: usize, ub: usize) -> (bool, TokenStream2) {
+        let mut result = TokenStream2::new();
+        let mut matched = false;
+        while let Some((token, cur)) = cursor.token_tree() {
+            cursor = cur;
+            match token {
+                ref tt @ TT::Punct(ref p) if p.as_char() == '#' => {
+                    cursor.clone().token_tree().map_or((), |(t, cur)| {
+                        let Some((TT::Punct(p), cur)) = cur.token_tree() else {
+                            result.append(tt.clone());
+                            return;
+                        };
+                        if p.as_char() != '*' {
+                            result.append(tt.clone());
+                            return;
+                        }
+                        matched = true;
+                        match t {
+                            TT::Group(g) => {
+                                cursor = cur;
+                                result.extend((lb..ub).into_iter().map(|val| Self::replace(TokenBuffer::new2(g.stream()).begin(), ident, val)));
+                            },
+                            _ => result.append(tt.clone()),
+                    }})
+                },
+                TT::Group(ref g) => {
+                    let (m, ts) = Self::partial_match(TokenBuffer::new2(g.stream()).begin(), ident, lb, ub);
+                    matched = m;
+                    let mut new_group = proc_macro2::Group::new(
+                        g.delimiter(),
+                        ts);
+                    new_group.set_span(g.span());
+                    result.append(new_group);
+                }
+                t => result.append(t),
+            };
+        }
+        (matched, result)
+    }
+
+    fn replace(mut cursor: Cursor, ident: &syn::Ident, val: usize) -> TokenStream2 {
         let mut result = TokenStream2::new();
         while let Some((token, cur)) = cursor.token_tree() {
             cursor = cur;
             let t = match token {
-                TT::Ident(i) if &i == ident => lit.clone().into(),
+                TT::Ident(i) if &i == ident => proc_macro2::Literal::usize_unsuffixed(val).into(),
+                ref tt @ TT::Ident(ref outter_id) => {
+                    cursor.clone().token_tree().map_or(tt.clone(), |(t, cur)| {
+                        match t {
+                            ref tt @ TT::Punct(ref p) if p.as_char() == '~' => {
+                                cursor = cur;
+                                cursor.clone().token_tree().map_or(tt.clone(), |(t, cur)| {
+                                    match t {
+                                        TT::Ident(i) if &i == ident => {
+                                            cursor = cur;
+                                            quote::format_ident!("{}{}", outter_id, val).into()
+                                        },
+                                        _ => tt.clone(),
+                                    }
+                                })
+                            },
+                            _ => tt.clone(),
+                        }
+                    })
+                },
                 TT::Group(ref g) => {
                     let mut new_group = proc_macro2::Group::new(
                         g.delimiter(),
-                        Self::replace(TokenBuffer::new2(g.stream()).begin(), ident, lit.clone()));
+                        Self::replace(TokenBuffer::new2(g.stream()).begin(), ident, val));
                     new_group.set_span(g.span());
                     new_group.into()
-                    },
+                },
                 t => t,
             };
             result.append(t);
