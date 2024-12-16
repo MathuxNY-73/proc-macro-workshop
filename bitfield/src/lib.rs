c36b80884fc44d7041fe654dac263f7cb3d4b004
Antoine Pouillaude <apouillaude@google.com>
2024-12-16 11:17:34 +0000

    Implements bit manipulation for u16
    * Refactor code to extract a shared interface for bit manipulations
      between u8 and u16.

--- bitfield/src/lib.rs
+++ bitfield/src/lib.rs
@@ -1,5 +1,3 @@
-use std::any;
-
 // Crates that have the "proc-macro" crate type are only allowed to export
 // procedural macros. So we cannot have one crate that defines procedural macros
 // alongside other types of public APIs like traits and structs.
@@ -15,8 +13,7 @@ use std::any;
 pub use bitfield_impl::{bitfield, gen};
 
 use thiserror::Error;
-use anyhow::{self, Context};
-use anyhow::bail;
+use anyhow::{self, bail};
 
 pub trait Specifier {
   const BITS: usize;
@@ -33,24 +30,59 @@ gen! {}
 #[error("Value {0} overflows the number of bits {BITS}")]
 struct OverflowError<const BITS: usize, T>(T);
 
-struct BitsU8<const BITS: usize, const START: usize, const SIZE: usize>;
+trait SetGet<const BITS: usize, const START: usize, const SIZE: usize> {
+  type Target;
 
-impl<const BITS: usize, const START: usize, const SIZE: usize> BitsU8<BITS, START, SIZE> {
   const START_IDX: usize = START / u8::BITS as usize;
   const END_IDX: usize = (START + BITS -  1) / u8::BITS as usize;
   const OFFSET: usize = START % u8::BITS as usize;
-  const IS_ACROSS: bool = Self::OFFSET + BITS > u8::BITS as usize;
+  const IS_ACROSS: bool;
 
-  const CHECK_: () = [()][(Self::END_IDX >= SIZE ||
-                           BITS > u8::BITS as usize) as usize];
-  const MASK_LHS: u8 =
-    if Self::OFFSET == 0 { 0 }
-    else { u8::MAX << (u8::BITS as usize - Self::OFFSET) };
+  const GET: fn(&[u8]) -> Self::Target;
+  const SET: fn(&mut [u8], Self::Target) -> anyhow::Result<()>;
+
+  const MASK_LHS: Self::Target;
   const MASK_RHS: u8 =
-    if Self::OFFSET == 0 && BITS == u8::BITS as usize { 0 }
+    if Self::OFFSET == 0 && BITS % 8 == 0 { 0 }
     else {
-      u8::MAX >> (BITS + Self::OFFSET
-                  - u8::BITS as usize * [0, 1][Self::IS_ACROSS as usize])};
+      u8::MAX >> ((BITS + Self::OFFSET) % 8)
+    };
+}
+
+trait CheckInBoundary<const BITS: usize, const START: usize, const SIZE: usize>: SetGet<BITS, START, SIZE> {
+  const CHECK: ();
+}
+
+impl<
+  const BITS: usize,
+  const START: usize,
+  const SIZE: usize,
+  T: SetGet<BITS, START, SIZE> + ?Sized> CheckInBoundary<BITS, START, SIZE> for T {
+    const CHECK: () = [()][(Self::END_IDX >= SIZE) as usize];
+}
+
+struct BitsU8<const BITS: usize, const START: usize, const SIZE: usize>;
+
+impl<const BITS: usize, const START: usize, const SIZE: usize> SetGet<BITS, START, SIZE> for BitsU8<BITS, START, SIZE> {
+  type Target = u8;
+
+  const MASK_LHS: u8 = if Self::OFFSET == 0 { 0 }
+    else { u8::MAX << (u8::BITS as usize - Self::OFFSET) };
+  const IS_ACROSS: bool = Self::OFFSET + BITS > u8::BITS as usize;
+  const GET: fn(&[u8]) -> Self::Target = if Self::IS_ACROSS {
+    Self::get_bits_accross
+  } else {
+    Self::get_bits
+  };
+  const SET: fn(&mut [u8], Self::Target) -> anyhow::Result<()> = if Self::IS_ACROSS {
+    Self::set_bits_accross
+  } else {
+    Self::set_bits
+  };
+}
+
+impl<const BITS: usize, const START: usize, const SIZE: usize> BitsU8<BITS, START, SIZE> {
+  const CHECK_: () = [()][(BITS > u8::BITS as usize) as usize];
 
   const fn does_fit(val: u8) -> bool {
     let val_num_bits = (u8::BITS - val.leading_zeros()) as usize;
@@ -63,7 +95,7 @@ impl<const BITS: usize, const START: usize, const SIZE: usize> BitsU8<BITS, STAR
       bail!(OverflowError::<BITS, u8>(val));
     }
 
-    let slice = &mut data[Self::START_IDX..=Self::START_IDX];
+    let slice = &mut data[Self::START_IDX..=Self::END_IDX];
     let old_val = u8::from_ne_bytes(slice.try_into().unwrap());
     let new_val = old_val & (Self::MASK_LHS ^ Self::MASK_RHS) | val << (u8::BITS as usize - BITS - Self::OFFSET);
     slice.copy_from_slice(&new_val.to_ne_bytes());
@@ -73,7 +105,7 @@ impl<const BITS: usize, const START: usize, const SIZE: usize> BitsU8<BITS, STAR
   fn get_bits(data: &[u8]) -> u8 {
     let _ = Self::CHECK_;
 
-    let slice = &data[Self::START_IDX..=Self::START_IDX];
+    let slice = &data[Self::START_IDX..=Self::END_IDX];
     let val = u8::from_ne_bytes(slice.try_into().unwrap());
     (val & !(Self::MASK_LHS ^ Self::MASK_RHS)) >> (u8::BITS as usize - BITS - Self::OFFSET)
   }
@@ -86,14 +118,14 @@ impl<const BITS: usize, const START: usize, const SIZE: usize> BitsU8<BITS, STAR
       bail!(OverflowError::<BITS, u8>(val));
     }
 
-    let lhs_slice = &mut data[Self::START_IDX..=Self::START_IDX];
+    let lhs_slice = &mut data[Self::START_IDX..Self::END_IDX];
     let lhs = u8::from_ne_bytes(lhs_slice.try_into().unwrap());
     let new_lhs = lhs & Self::MASK_LHS | val >> (BITS + Self::OFFSET - u8::BITS as usize);
     lhs_slice.copy_from_slice(&new_lhs.to_ne_bytes());
 
     let rhs_slice = &mut data[Self::END_IDX..=Self::END_IDX];
     let rhs = u8::from_ne_bytes(rhs_slice.try_into().unwrap());
-    let new_rhs = rhs & Self::MASK_RHS | val << (2 * u8::BITS as usize - BITS - Self::OFFSET);
+    let new_rhs = rhs & Self::MASK_RHS | val << (u8::BITS as usize - ((BITS + Self::OFFSET) % 8));
     rhs_slice.copy_from_slice(&new_rhs.to_ne_bytes());
 
     Ok(())
@@ -102,27 +134,127 @@ impl<const BITS: usize, const START: usize, const SIZE: usize> BitsU8<BITS, STAR
   fn get_bits_accross(data: &[u8]) -> u8 {
     let _ = Self::CHECK_;
 
-    let lhs_slice = &data[Self::START_IDX..=Self::START_IDX];
+    let lhs_slice = &data[Self::START_IDX..Self::END_IDX];
     let lhs = u8::from_ne_bytes(lhs_slice.try_into().unwrap());
     let lhs = (lhs & !Self::MASK_LHS) << (BITS + Self::OFFSET - u8::BITS as usize);
 
     let rhs_slice = &data[Self::END_IDX..=Self::END_IDX];
     let rhs = u8::from_ne_bytes(rhs_slice.try_into().unwrap());
-    let rhs = (rhs & !Self::MASK_RHS) >> (2 * u8::BITS as usize - BITS - Self::OFFSET);
+    let rhs = (rhs & !Self::MASK_RHS) >> (u8::BITS as usize - ((BITS + Self::OFFSET) % 8));
 
     lhs | rhs
   }
 
 }
 
+struct BitsU16<const BITS: usize, const START: usize, const SIZE: usize>;
 
+impl<const BITS: usize, const START: usize, const SIZE: usize> SetGet<BITS, START, SIZE> for BitsU16<BITS, START, SIZE> {
+  type Target = u16;
+
+  const MASK_LHS: Self::Target = if Self::OFFSET == 0 { 0 }
+    else { u16::MAX << (u16::BITS as usize - Self::OFFSET) };
+
+  const IS_ACROSS: bool = Self::OFFSET + BITS > u16::BITS as usize;
+  const GET: fn(&[u8]) -> Self::Target = if Self::IS_ACROSS {
+    Self::get_bits_accross
+  } else {
+    Self::get_bits
+  };
+  const SET: fn(&mut [u8], Self::Target) -> anyhow::Result<()> = if Self::IS_ACROSS {
+    Self::set_bits_accross
+  } else {
+    Self::set_bits
+  };
+}
+
+impl<const BITS: usize, const START: usize, const SIZE: usize> BitsU16<BITS, START, SIZE> {
+  const CHECK_: () = [()][(BITS > u16::BITS as usize ||
+                           BITS <= u8::BITS as usize) as usize];
+  const READ: fn([u8; 2]) -> u16 = if cfg!(target_endian = "big") {
+    u16::from_le_bytes
+  } else {
+    u16::from_be_bytes
+  };
+  const WRITE: fn(u16) -> [u8; 2] = if cfg!(target_endian = "big") {
+    u16::to_le_bytes
+  } else {
+    u16::to_be_bytes
+  };
+
+  const fn does_fit(val: u16) -> bool {
+    let val_num_bits = (u16::BITS - val.leading_zeros()) as usize;
+    val_num_bits <= BITS
+  }
+
+  fn set_bits(data: &mut [u8], val: u16) -> anyhow::Result<()> {
+    let _ = Self::CHECK_;
+    if !Self::does_fit(val) {
+      bail!(OverflowError::<BITS, u16>(val));
+    }
+
+    let slice = &mut data[Self::START_IDX..=Self::END_IDX];
+    let old_val = Self::READ(slice.try_into().unwrap());
+    let new_val = old_val & (Self::MASK_LHS ^ Self::MASK_RHS as u16) | val << (u16::BITS as usize - BITS - Self::OFFSET);
+    slice.copy_from_slice(&Self::WRITE(new_val));
+    Ok(())
+  }
+
+  fn get_bits(data: &[u8]) -> u16 {
+    let _ = Self::CHECK_;
+
+    let slice = &data[Self::START_IDX..=Self::END_IDX];
+    let val = Self::READ(slice.try_into().unwrap());
+    // println!("lhs = {:016b}, rhs = {:016b}, lhs ^ rhs = {:016b}", Self::MASK_LHS, Self::MASK_RHS, Self::MASK_LHS ^ Self::MASK_RHS as u16);
+    // println!("{:016b}", !(Self::MASK_LHS ^ Self::MASK_RHS as u16));
+    // println!("val = {:016b}, res={:016b}", val, (val & !(Self::MASK_LHS ^ Self::MASK_RHS as u16)));
+    // println!("shift = {}", (u16::BITS as usize - BITS - Self::OFFSET));
+    // println!("res={:016b}", (val & !(Self::MASK_LHS ^ Self::MASK_RHS as u16)) >> (u16::BITS as usize - BITS - Self::OFFSET));
+    (val & !(Self::MASK_LHS ^ Self::MASK_RHS as u16)) >> (u16::BITS as usize - BITS - Self::OFFSET)
+  }
+
+  fn set_bits_accross(
+      data: &mut [u8], val: u16) -> anyhow::Result<()> {
+    let _ = Self::CHECK_;
+
+    if !Self::does_fit(val) {
+      bail!(OverflowError::<BITS, u16>(val));
+    }
+
+    let lhs_slice = &mut data[Self::START_IDX..Self::END_IDX];
+    let lhs = Self::READ(lhs_slice.try_into().unwrap());
+    let new_lhs = lhs & Self::MASK_LHS | val >> (BITS + Self::OFFSET - u16::BITS as usize);
+    lhs_slice.copy_from_slice(&Self::WRITE(new_lhs));
+
+    let rhs_slice = &mut data[Self::END_IDX..=Self::END_IDX];
+    let rhs = u8::from_ne_bytes(rhs_slice.try_into().unwrap());
+    let new_rhs = rhs & Self::MASK_RHS | ((val & u8::MAX as u16) as u8) << (u8::BITS as usize - ((BITS + Self::OFFSET) % 8));
+    rhs_slice.copy_from_slice(&new_rhs.to_ne_bytes());
+
+    Ok(())
+  }
+
+  fn get_bits_accross(data: &[u8]) -> u16 {
+    let _ = Self::CHECK_;
+
+    let lhs_slice = &data[Self::START_IDX..Self::END_IDX];
+    let lhs = Self::READ(lhs_slice.try_into().unwrap());
+    let lhs = (lhs & !Self::MASK_LHS) << (BITS + Self::OFFSET - u16::BITS as usize);
+
+    let rhs_slice = &data[Self::END_IDX..=Self::END_IDX];
+    let rhs = u8::from_ne_bytes(rhs_slice.try_into().unwrap());
+    let rhs = (rhs & !Self::MASK_RHS) >> (u8::BITS as usize - ((BITS + Self::OFFSET) % 8));
+
+    lhs | (rhs as u16)
+  }
+}
 
 #[cfg(test)]
 mod tests {
   use super::*;
 
   #[test]
-  fn test_get_bits() {
+  fn test_get_bits_u8() {
     let data = [
       0b10101010,
       0b11110000,
@@ -136,14 +268,14 @@ mod tests {
     assert_eq!(BitsU8::<3, 20, 5>::OFFSET, 4);
     assert_eq!(BitsU8::<3, 20, 5>::MASK_LHS, 0b11110000);
     assert_eq!(BitsU8::<3, 20, 5>::MASK_RHS, 0b00000001);
-    assert_eq!(BitsU8::<3, 20, 5>::get_bits(&data), 0b00000001);
+    assert_eq!(BitsU8::<3, 20, 5>::GET(&data), 0b00000001);
 
     assert_eq!(BitsU8::<8, 32, 5>::END_IDX, 4);
     assert_eq!(BitsU8::<8, 32, 5>::START_IDX, 4);
     assert_eq!(BitsU8::<8, 32, 5>::OFFSET, 0);
     assert_eq!(BitsU8::<8, 32, 5>::MASK_LHS, 0b0000000);
     assert_eq!(BitsU8::<8, 32, 5>::MASK_RHS, 0b0000000);
-    assert_eq!(BitsU8::<8, 32, 5>::get_bits(&data), 0b00111100);
+    assert_eq!(BitsU8::<8, 32, 5>::GET(&data), 0b00111100);
 
     // Accross u8
     assert_eq!(BitsU8::<8, 20, 5>::END_IDX, 3);
@@ -151,18 +283,91 @@ mod tests {
     assert_eq!(BitsU8::<8, 20, 5>::OFFSET, 4);
     assert_eq!(BitsU8::<8, 20, 5>::MASK_LHS, 0b11110000);
     assert_eq!(BitsU8::<8, 20, 5>::MASK_RHS, 0b00001111);
-    assert_eq!(BitsU8::<8, 20, 5>::get_bits_accross(&data), 0b00111100);
+    assert_eq!(BitsU8::<8, 20, 5>::GET(&data), 0b00111100);
 
     assert_eq!(BitsU8::<2, 7, 5>::END_IDX, 1);
     assert_eq!(BitsU8::<2, 7, 5>::START_IDX, 0);
     assert_eq!(BitsU8::<2, 7, 5>::OFFSET, 7);
     assert_eq!(BitsU8::<2, 7, 5>::MASK_LHS, 0b11111110);
     assert_eq!(BitsU8::<2, 7, 5>::MASK_RHS, 0b01111111);
-    assert_eq!(BitsU8::<2, 7, 5>::get_bits_accross(&data), 0b00000001);
+    assert_eq!(BitsU8::<2, 7, 5>::GET(&data), 0b00000001);
   }
 
   #[test]
-  fn test_set_bits() {
+  fn test_get_bits_u16() {
+    let data = [0xAA, 0xF0, 0x33, 0xCC, 0x3C];
+
+    // Inside 2 u8s
+    assert_eq!(BitsU16::<10, 20, 5>::END_IDX, 3);
+    assert_eq!(BitsU16::<10, 20, 5>::START_IDX, 2);
+    assert_eq!(BitsU16::<10, 20, 5>::OFFSET, 4);
+    assert_eq!(BitsU16::<10, 20, 5>::MASK_LHS, 0xF000);
+    assert_eq!(BitsU16::<10, 20, 5>::MASK_RHS, 0b00000011);
+    assert_eq!(BitsU16::<10, 20, 5>::GET(&data), 0x00F3);
+
+    assert_eq!(BitsU16::<16, 24, 5>::END_IDX, 4);
+    assert_eq!(BitsU16::<16, 24, 5>::START_IDX, 3);
+    assert_eq!(BitsU16::<16, 24, 5>::OFFSET, 0);
+    assert_eq!(BitsU16::<16, 24, 5>::MASK_LHS, 0x0000);
+    assert_eq!(BitsU16::<16, 24, 5>::MASK_RHS, 0b0000000);
+    assert_eq!(BitsU16::<16, 24, 5>::GET(&data), 0xCC3C);
+
+    // Accross 3 u8s
+    assert_eq!(BitsU16::<16, 20, 5>::END_IDX, 4);
+    assert_eq!(BitsU16::<16, 20, 5>::START_IDX, 2);
+    assert_eq!(BitsU16::<16, 20, 5>::OFFSET, 4);
+    assert_eq!(BitsU16::<16, 20, 5>::MASK_LHS, 0xF000);
+    assert_eq!(BitsU16::<16, 20, 5>::MASK_RHS, 0b00001111);
+    assert_eq!(BitsU16::<16, 20, 5>::GET(&data), 0x3CC3);
+
+    assert_eq!(BitsU16::<10, 7, 5>::END_IDX, 2);
+    assert_eq!(BitsU16::<10, 7, 5>::START_IDX, 0);
+    assert_eq!(BitsU16::<10, 7, 5>::OFFSET, 7);
+    assert_eq!(BitsU16::<10, 7, 5>::MASK_LHS, 0xFE00);
+    assert_eq!(BitsU16::<10, 7, 5>::MASK_RHS, 0b01111111);
+    assert_eq!(BitsU16::<10, 7, 5>::GET(&data), 0x01E0);
+  }
+
+  fn print_data<'a>(data: impl IntoIterator<Item = &'a u8>) {
+    eprintln!("[{}]",
+      data
+        .into_iter()
+        .map(|d| format!("{d:#02x}")).collect::<Vec<_>>().join(","));
+  }
+
+  #[test]
+  fn test_set_bits_u16() {
+    let mut data = [0xAA, 0xF0, 0x33, 0xCC, 0x3C];
+
+    print_data(&data);
+
+    // Inside 2 u8s
+    assert!(BitsU16::<10, 16, 5>::SET(&mut data, 0x0156).is_ok());
+    assert_eq!(BitsU16::<10, 16, 5>::GET(&data), 0x0156);
+
+    print_data(&data);
+
+    assert!(BitsU16::<9, 3, 5>::SET(&mut data, 0x015D).is_ok());
+    assert_eq!(BitsU16::<9, 3, 5>::GET(&data), 0x015D);
+
+    print_data(&data);
+
+    // Accross 3 u8s
+    assert!(BitsU16::<16, 20, 5>::SET(&mut data, 0xD528).is_ok());
+    assert_eq!(BitsU16::<16, 20, 5>::GET(&data), 0xD528);
+
+    print_data(&data);
+
+    assert!(BitsU16::<10, 7, 5>::SET(&mut data, 0x02B7).is_ok());
+    assert_eq!(BitsU16::<10, 7, 5>::GET(&data), 0x02B7);
+
+    print_data(&data);
+
+    assert_eq!(data, [0xB5, 0x5B, 0xDD, 0x52, 0x8C]);
+  }
+
+  #[test]
+  fn test_set_bits_u8() {
     let mut data = [
       0b10101010,
       0b11110000,
@@ -171,22 +376,22 @@ mod tests {
       0b00111100];
 
     // Inside u8
-    assert_eq!(BitsU8::<4, 16, 5>::get_bits(&data), 0b00000011);
-    assert!(BitsU8::<4, 16, 5>::set_bits(&mut data, 0b00001010).is_ok());
-    assert_eq!(BitsU8::<4, 16, 5>::get_bits(&data), 0b00001010);
+    assert_eq!(BitsU8::<4, 16, 5>::GET(&data), 0b00000011);
+    assert!(BitsU8::<4, 16, 5>::SET(&mut data, 0b00001010).is_ok());
+    assert_eq!(BitsU8::<4, 16, 5>::GET(&data), 0b00001010);
 
-    assert_eq!(BitsU8::<8, 0, 5>::get_bits(&data), 0b10101010);
-    assert!(BitsU8::<8, 0, 5>::set_bits(&mut data, 0b11100011).is_ok());
-    assert_eq!(BitsU8::<8, 0, 5>::get_bits(&data), 0b11100011);
+    assert_eq!(BitsU8::<8, 0, 5>::GET(&data), 0b10101010);
+    assert!(BitsU8::<8, 0, 5>::SET(&mut data, 0b11100011).is_ok());
+    assert_eq!(BitsU8::<8, 0, 5>::GET(&data), 0b11100011);
 
     // Accross u8
-    assert_eq!(BitsU8::<8, 20, 5>::get_bits_accross(&data), 0b00111100);
-    assert!(BitsU8::<8, 20, 5>::set_bits_accross(&mut data, 0b01001001).is_ok());
-    assert_eq!(BitsU8::<8, 20, 5>::get_bits_accross(&data), 0b01001001);
+    assert_eq!(BitsU8::<8, 20, 5>::GET(&data), 0b00111100);
+    assert!(BitsU8::<8, 20, 5>::SET(&mut data, 0b01001001).is_ok());
+    assert_eq!(BitsU8::<8, 20, 5>::GET(&data), 0b01001001);
 
-    assert_eq!(BitsU8::<2, 7, 5>::get_bits_accross(&data), 0b00000011);
-    assert!(BitsU8::<2, 7, 5>::set_bits_accross(&mut data, 0b00000001).is_ok());
-    assert_eq!(BitsU8::<2, 7, 5>::get_bits_accross(&data), 0b00000001);
+    assert_eq!(BitsU8::<2, 7, 5>::GET(&data), 0b00000011);
+    assert!(BitsU8::<2, 7, 5>::SET(&mut data, 0b00000001).is_ok());
+    assert_eq!(BitsU8::<2, 7, 5>::GET(&data), 0b00000001);
 
     assert_eq!(data, [
       0b11100010,
