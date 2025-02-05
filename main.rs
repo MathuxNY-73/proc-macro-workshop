399ac11699d317e140d22c66cc35889c808bde6c
Antoine Pouillaude <apouillaude@google.com>
2025-02-05 13:58:11 +0000

    Solved exercises 6 and 7 of bitfield proc-macro
     * There is still a problem with out of range variant. eg: setting `G`
       to 8 in exercise will lead to a runtime error.

--- main.rs
+++ main.rs
@@ -10,28 +10,23 @@ use bitfield::*;
 
 #[bitfield]
 pub struct RedirectionTableEntry {
-    acknowledged: bool,
-    trigger_mode: TriggerMode,
     delivery_mode: DeliveryMode,
-    reserved: B3,
+    reserved: B5,
 }
 
-#[derive(BitfieldSpecifier, Debug, PartialEq)]
-pub enum TriggerMode {
-    Edge = 0,
-    Level = 1,
-}
+const F: isize = 3;
+const G: isize = 8;
 
 #[derive(BitfieldSpecifier, Debug, PartialEq)]
 pub enum DeliveryMode {
-    Fixed = 0b000,
-    Lowest = 0b001,
-    SMI = 0b010,
-    RemoteRead = 0b011,
-    NMI = 0b100,
-    Init = 0b101,
-    Startup = 0b110,
-    External = 0b111,
+    Fixed = F,
+    Lowest,
+    SMI,
+    RemoteRead,
+    NMI,
+    Init = G,
+    Startup,
+    External,
 }
 
 fn main() {
@@ -39,13 +34,8 @@ fn main() {
 
     // Initialized to all 0 bits.
     let mut entry = RedirectionTableEntry::new();
-    assert_eq!(entry.get_acknowledged(), false);
-    assert_eq!(entry.get_trigger_mode(), TriggerMode::Edge);
-    assert_eq!(entry.get_delivery_mode(), DeliveryMode::Fixed);
+    assert_eq!(entry.get_delivery_mode(), DeliveryMode::Init);
 
-    entry.set_acknowledged(true);
-    entry.set_delivery_mode(DeliveryMode::SMI);
-    assert_eq!(entry.get_acknowledged(), true);
-    assert_eq!(entry.get_trigger_mode(), TriggerMode::Edge);
-    assert_eq!(entry.get_delivery_mode(), DeliveryMode::SMI);
+    entry.set_delivery_mode(DeliveryMode::Lowest);
+    assert_eq!(entry.get_delivery_mode(), DeliveryMode::Lowest);
 }
